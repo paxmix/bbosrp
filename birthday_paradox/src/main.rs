381200497114f9509d876b810f0f2a5e2fb36001
paxmix <manh1998nb@gmail.com>
2025-11-15 17:33:06 +0700

    implement if let to code

--- birthday_paradox/src/main.rs
+++ birthday_paradox/src/main.rs
@@ -20,24 +20,25 @@ simulations) to explore this concept.
 
     println!();
 
-    let mut num_birthdays = String::new();
-    loop {
+    let num_birthdays: u32 = loop {
         println!("How many birthdays shall I generate? (Max 100)");
         print!("> ");
         io::stdout().flush().unwrap();
 
+        let mut input = String::new();
+
         io::stdin()
-            .read_line(&mut num_birthdays)
+            .read_line(&mut input)
             .expect("Failed to read line!");
-        let num_birthdays_i32 = num_birthdays.trim().parse::<i32>();
-        if num_birthdays_i32.is_ok() && num_birthdays_i32.unwrap() <= 100 {
-            break;
-        } else {
-            println!("Invalid input!\n");
-            num_birthdays.clear();
+
+        if let Ok(n) = input.trim().parse::<u32>()
+            && n <= 100
+        {
+            break n;
         }
-    }
-    let num_birthdays: i32 = num_birthdays.trim().parse().unwrap();
+        input.clear();
+        println!("Invalid input!");
+    };
 
     let random_birthdays = paradox_simulation(num_birthdays);
     println!("Here are {} birthdays:", num_birthdays);
@@ -74,7 +75,7 @@ simulations) to explore this concept.
     _ = io::stdin()
         .read_line(&mut String::new())
         .expect("Failed to read line.");
-    println!("Let's rung another {} simulations.", SIMU_NUMS);
+    println!("Let's run another {} simulations.", SIMU_NUMS);
 
     let mut simu_done = 0;
     let mut counter = 0;
@@ -111,14 +112,15 @@ fn check_dup_birthdays(random_birthdays: Vec<NaiveDate>) -> Option<NaiveDate> {
         .find(|&day| !dup_birthdays.insert(day))
 }
 
-fn paradox_simulation(num_birthdays: i32) -> Vec<NaiveDate> {
+fn paradox_simulation(num_birthdays: u32) -> Vec<NaiveDate> {
     let mut random_birthdays = Vec::new();
     let mut rng = rand::rng();
 
     while random_birthdays.len() < num_birthdays.try_into().unwrap() {
-        let day = NaiveDate::from_yo_opt(2001, rng.random_range(1..=365)).unwrap();
-
-        random_birthdays.push(day);
+        if let Some(day) = NaiveDate::from_yo_opt(2001, rng.random_range(1..=365)) {
+            random_birthdays.push(day);
+        }
     }
+
     random_birthdays
 }
